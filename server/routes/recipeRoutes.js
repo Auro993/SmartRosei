c01537f2396530f1a4b6ae8e68ef12a0398a7da9
Aurosmita Sahoo <sahooaurosmita204@gmail.com>
2026-04-11 21:46:19 +0530

    Update recipeRoutes.js

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { getRecipes } = require("../controllers/recipeController");
 
 const router = express.Router();
-
+ 
 router.get("/recipes", getRecipes);
 
 module.exports = router;
